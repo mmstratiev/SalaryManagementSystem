a71e05e504f7050f47219f3ac3160ee8f63366d7
mmstratiev <miroslav_stratiev@abv.bg>
2020-02-08 16:53:01 +0200

    Add project files.

---
+++ SalaryManagementSystem/Database/EmployeesDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Common;
+using System.Configuration;
+
+namespace SalaryManagementSystem
+{
+    class EmployeesDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ SalaryManagementSystem/Model/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryManagementSystem
+{
+    class Employee
+    {
+        [Key]
+        public int      ID      { get; set; }
+        [Required]
+        public string   Name    { get; set; }
+        [Required]
+        public string   EGN     { get; set; }
+        [Required]
+        public double   Salary  { get; set; }
+    }
+}
