44b82e174c8f80e61078a1b0bfab7e277e31bf23
gerdjikov17 <gerdjikov17@abv.bg>
2020-02-09 13:43:45 +0200

    Refactor: update namespaces

--- SalaryManagementSystem/Model/EmployeeSalaryBill.cs
+++ SalaryManagementSystem/Model/EmployeeSalaryBill.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SalaryManagementSystem.Constants;
 
-namespace SalaryManagementSystem.Model
+namespace SalaryManagementSystem
 {
     class EmployeeSalaryBill
     {
@@ -25,14 +24,14 @@ namespace SalaryManagementSystem.Model
             this.CompanyName = companyName ?? throw new ArgumentNullException(nameof(companyName));
             this.Date = date;
 
-            double SalaryForTaxes = employee.Salary > Constants.Constants.MAX_INSURANCE_INCOME ? Constants.Constants.MAX_INSURANCE_INCOME : employee.Salary;
+            double SalaryForTaxes = employee.Salary > Constants.MAX_INSURANCE_INCOME ? Constants.MAX_INSURANCE_INCOME : employee.Salary;
 
             //Calculate taxes
-            this.DDFL = employee.Salary * (Constants.Constants.DDFL_PERCENTAGE / 100);
-            this.ZO = SalaryForTaxes * (Constants.Constants.ZO_PERCENTAGE / 100);
-            this.DOO_PENSII = SalaryForTaxes * (Constants.Constants.DOO_PENSII_PERCENTAGE / 100);
-            this.DOO_ZOM = SalaryForTaxes * (Constants.Constants.DOO_ZOM_PERCENTAGE / 100);
-            this.DOO_BEZRABOTICA = SalaryForTaxes * (Constants.Constants.DOO_BEZRABOTICA_PERCENTAGE / 100);
+            this.DDFL = employee.Salary * (Constants.DDFL_PERCENTAGE / 100);
+            this.ZO = SalaryForTaxes * (Constants.ZO_PERCENTAGE / 100);
+            this.DOO_PENSII = SalaryForTaxes * (Constants.DOO_PENSII_PERCENTAGE / 100);
+            this.DOO_ZOM = SalaryForTaxes * (Constants.DOO_ZOM_PERCENTAGE / 100);
+            this.DOO_BEZRABOTICA = SalaryForTaxes * (Constants.DOO_BEZRABOTICA_PERCENTAGE / 100);
 
             //NET SALARY
             this.NetSalary = employee.Salary - (this.DDFL + this.ZO + this.DOO_PENSII + this.DOO_ZOM + this.DOO_BEZRABOTICA);
